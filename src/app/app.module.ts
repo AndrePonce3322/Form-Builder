ccce9e9c8bea55416e362850d4e1a3ab4c5fb49b
AndrePonce3322 <andreponce417@gmail.com>
2023-04-01 23:14:06 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+// Peticiones HTTP
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RoutingModule } from './routing/routing.module';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
